8e5125dc8afe08ca57d539d9563952a76a92317c
umaepoch <umag@epochconsulting.in>
2020-06-26 12:46:14 +0530

    biometric updates

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+#from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in biometric_attendance/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('requirements.txt) as f:
-install_requires = f.read().strip().split(’\n’)
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 with open('biometric_attendance/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-#requirements = parse_requirements("requirements.txt", session="")
+requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='biometric_attendance',
@@ -23,7 +26,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires= install_requires
-	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
-
