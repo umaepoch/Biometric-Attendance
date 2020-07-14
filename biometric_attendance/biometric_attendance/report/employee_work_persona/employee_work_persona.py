ff1dc42b93c540d63d34162c21f6c4e72fbd1eed
umaepoch <umag@epochconsulting.in>
2020-07-14 18:05:09 +0530

    testing

--- biometric_attendance/biometric_attendance/report/employee_work_persona/employee_work_persona.py
+++ biometric_attendance/biometric_attendance/report/employee_work_persona/employee_work_persona.py
@@ -295,5 +295,3 @@ def get_status(employee,date):
 	status = frappe.db.sql("""select * from `tabBiometric Attendance` where employee_id = %s and date =%s order by name desc limit 1 """,(employee,date), as_dict=1)
 	return status
 
-
-
