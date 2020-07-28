2c728b0f67fb44ce7226f2b019c40ecc3d69bb3b
SureshAlien <sureshnaga96@gmail.com>
2020-07-28 15:51:05 +0530

    leaflet

--- biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.js
+++ biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.js
@@ -3,6 +3,10 @@
 
 frappe.ui.form.on('Biometric Attendance', {
 	refresh: function(frm) {
-
+		console.log("ref working latField:"+frm.doc.latitude+"  lonField: "+frm.doc.longitude);
+		frm.fields_dict.map_view.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
+		console.log(" lat val from map"+frm.fields_dict.map_view.map.getCenter()['lat']);
+		console.log(" lng val from map"+frm.fields_dict.map_view.map.getCenter()['lng']);
 	}
 });
+
