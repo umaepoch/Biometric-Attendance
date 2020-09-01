c831f919ad319cbfc7832de078e681cbe1ba242b
umaepoch <umag@epochconsulting.in>
2020-09-01 15:01:35 +0530

    biometric attendace

--- biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.py
+++ biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.py
@@ -5,40 +5,42 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import json
 
 class BiometricAttendance(Document):
 	pass
 
 @frappe.whitelist()
 def add_log_based_on_employee_field():
-	reqData = json.loads(frappe.request.data)
-	user_id = reqData.get("employee_field_value")
-	device_name = reqData.get("device_id")
-	timestamp = reqData.get("timestamp")
-	log_type = reqData.get("log_type")
-	attendance_doc = frappe.new_doc("Biometric Attendance")
-	punch = 0
-	Status = ""
-	if log_type == "IN":
+	if frappe.request.data:
+		reqData = json.loads(frappe.request.data)
+		user_id = reqData.get("employee_field_value")
+		device_name = reqData.get("device_id")
+		timestamp = reqData.get("timestamp")
+		log_type = reqData.get("log_type")
+		attendance_doc = frappe.new_doc("Biometric Attendance")
 		punch = 0
-		Status = "Check In"
-	elif log_type == "OUT":
-		punch = 1
-		Status = "Check Out"
-	else:
-		punch = 10
-		Status = "Unknown"
-	attendance_doc.user_id = user_id
-	attendance_doc.timestamp = timestamp
-	attendance_doc.punch = punch
-	attendance_doc.status = Status
-	attendance_doc.date = timestamp_date
-	attendance_doc.source = device_name
-	#attendance_doc.punch_status = "Before Lunch"
+		Status = ""
+		if log_type == "IN":
+			punch = 0
+			Status = "Check In"
+		elif log_type == "OUT":
+			punch = 1
+			Status = "Check Out"
+		else:
+			punch = 10
+			Status = "Unknown"
+		attendance_doc.user_id = user_id
+		attendance_doc.timestamp = timestamp
+		attendance_doc.punch = punch
+		attendance_doc.status = Status
+		attendance_doc.date = timestamp_date
+		attendance_doc.source = device_name
+		#attendance_doc.punch_status = "Before Lunch"
 	
-	attendance_doc.save()
-	attendance_doc.submit()
-	if attendance_doc.name:
-		return "Success"
-	else:
-		return  False
+		attendance_doc.save()
+		attendance_doc.submit()
+		if attendance_doc.name:
+			return "Success"
+		else:
+			return  False
