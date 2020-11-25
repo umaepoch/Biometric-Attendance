e7209d20fc04a36134358e444f538813f0640cdb
SureshAlien <sureshnaga96@gmail.com>
2020-11-25 15:39:50 +0530

    map erp

--- biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.js
+++ biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance.js
@@ -26,6 +26,7 @@ frappe.ui.form.on('Biometric Attendance', {
 
 		//console.log("ref working latField:"+frm.doc.latitude+"  lonField: "+frm.doc.longitude);
 		frm.fields_dict.map_view.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
+		frm.fields_dict.map_view_e.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
 		//console.log(" lat val from map"+frm.fields_dict.map_view.map.getCenter()['lat']);
 		//console.log(" lng val from map"+frm.fields_dict.map_view.map.getCenter()['lng']);
 
